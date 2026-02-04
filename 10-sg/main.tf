091bfaccb64034f60ca2082452ae040ae7f8f001
Your Name <your.email@example.com>
2026-02-04 21:27:12 +0530

    Quick update

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,10 +1,18 @@
-module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+#open source module
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
+#   name        = "${local.common_name_suffix}-catalogue"
+#   use_name_prefix = false
+#   description = "Security group for catalogue with custom ports open within VPC, and egress all traffic "
+#   vpc_id      = data.aws_ssm_parameter.vpc-id.value   #ssm parameter store key-value pair 
+# }
 
-  name        = "${local.common_name_suffix}-catalogue"
-  use_name_prefix = false
-  description = "Security group for catalogue with custom ports open within VPC, and egress all traffic "
-  vpc_id      = data.aws_ssm_parameter.vpc-id.value   #ssm parameter store key-value pair
+module "sg" {
+  source = "https://github.com/ashokkumar1112090/terraform-aws-sg.git:ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  sg_id = "mongodb"
+  sg_description = "created for mongodb"
+  vpc_id = local.vpc_id
 
-  
 }
