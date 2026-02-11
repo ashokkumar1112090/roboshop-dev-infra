7ce9e115260555db02bf2da922e3c80737351871
Your Name <your.email@example.com>
2026-02-11 10:04:42 +0530

    Quick update

--- 40-databases/bootstarp.sh
+++ 40-databases/bootstarp.sh
@@ -1,6 +1,9 @@
 #1/bin/bash          #shell-scripting concept
-dnf install ansible -y
-ansible-pull -U https://github.com/ashokkumar1112090/ansible-roboshop-roles.tf.git -e component=mongodb main.yaml
+yum install ansible -y
+ansible-pull -U https://github.com/ashokkumar1112090/ansible-roboshop-roles.tf.git \
+  -e component=mongodb \
+  main.yaml
+
 # git clone ansible-playbook
 # cd ansible-playbook
 # ansible-playbook -i inventory main.yaml
