224d0a97c8b438675919c9906fd0c62ba0e3f72a
Your Name <your.email@example.com>
2026-02-16 09:02:52 +0530

    Quick update

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,8 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     common_name_suffix = "${var.project_name}-${var.environment}"
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-    private_subnet_ids= split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id= split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids= split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
     project = var.project_name
     environment = var.environment
