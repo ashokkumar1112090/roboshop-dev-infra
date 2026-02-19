c553c7894988547a13fa20166446812ee7be01dc
Your Name <your.email@example.com>
2026-02-19 14:46:45 +0530

    Quick update

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,11 +1,21 @@
 data "aws_ami" "joindevops" {
-  owners      = ["973714476881"]
-  most_recent = true
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["Redhat-9-DevOps-Practice"]
+    }
 
-  filter {
-    name   = "name"
-    values = ["Redhat-9-DevOps-Practice*"]
-  }
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
